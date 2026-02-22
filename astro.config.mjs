2d20d5e10ad9739e1185c4a8bc0abcb39e829e80
anawat34115 <85146954+anawat34115@users.noreply.github.com>
2026-02-23 01:21:58 +0700

    Add Decap CMS admin panel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-
-import keystatic from '@keystatic/astro';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://animebulk.com',
-  output: 'hybrid',
-  adapter: vercel(),
   integrations: [
     tailwind(),
-    react(),
-    keystatic(),
   ],
 });
